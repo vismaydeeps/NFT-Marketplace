b3177ccdf52db52b20ae204d7d956f3b45d5f8f9
Dabbin-Giraffe <shashank.kathuri@gmail.com>
2024-11-29 00:13:15 +0530

    bidding history progress

--- front/src/components/Auctions/ViewBids/ViewBids.jsx
+++ front/src/components/Auctions/ViewBids/ViewBids.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./ViewBids.css";
 import tempNFT from "../../../assets/tempNFT.png";
+import ViewBidsHistory from './ViewBidsHistory/ViewBidsHistory';
 
 
 const ViewBids = () => {
@@ -15,20 +16,24 @@ const ViewBids = () => {
               <p className="view-bids-user">user</p>
             </div>
             <div className="bids-data-bottom">
-                <div className="bids-data-timer">
-                  <p>18:00:00</p>
-                </div>
-                <form action="" className="view-bids">
-                  <input type="text" placeholder='Enter your bid'/>
-                  <button type="submit">Place Bid</button>
-                </form>
+              <div className="bids-data-timer">
+                <p>18:00:00</p>
+              </div>
             </div>
-
+          </div>
+          <div className="bids-highest">
+            <p>Largest Bid Placed : <span>123 ETH</span>
+            </p>
           </div>
         </div>
-
-        <div className="view-mid">
-
+        <div className="view-bids-mid">
+          <form action="" className="view-bids">
+            <input type="text" placeholder='Enter your bid (ETH)' />
+            <button type="submit">Place Bid</button>
+          </form>
+        </div>
+        <div className="view-bids-bottom">
+          <ViewBidsHistory />
         </div>
       </div >
 
---
+++ front/src/components/Auctions/ViewBids/ViewBidsHistory/ViewBidsHistory.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./ViewBidsHistory.css";
+import bidsHistory from "./mockBidsHistory.json";
+
+const ViewBidsHistory = (props) => {
+
+  const bids = bidsHistory.auctions[0].bids;
+
+  return (
+    <>
+      <div className="bids-history-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th className='start'>Rank</th>
+              <th className='mid'>User</th>
+              <th className='mid'>Bidding Amount</th>
+              <th className='end'>Time of Bid</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              bids.map((bid, index) => (
+                <tr>
+                  <td className='start'>{index + 1}</td>
+                  <td className='mid'>{bid.bidderId}</td>
+                  <td className='mid'>{bid.amount} ETH</td>
+                  <td className='end'>{bid.timestamp}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default ViewBidsHistory
