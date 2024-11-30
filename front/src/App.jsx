96e92af076131e117ca2743e0e6225f6719ee511
Dabbin-Giraffe <shashank.kathuri@gmail.com>
2024-11-30 22:54:12 +0530

    started work on Create nft

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,6 +7,7 @@ import CreateAccount from './components/CreateAccount/CreateAccount';
 import Auctions from './components/Auctions/Auctions';
 import ViewBids from './components/Auctions/ViewBids/ViewBids';
 import Sale from './components/SalePage/Sale';
+import CreateNFT from './components/CreateNFT/CreateNFT';
 import User from './components/User/User';
 
 const App = () => {
@@ -17,7 +18,8 @@ const App = () => {
         {/* <LeaderBoard /> */}
         {/* <Auctions /> */}
         {/* <Sale /> */}
-        <User/>
+        {/* <User/> */}
+        <CreateNFT />
       </div>
       {/* <Login /> */}
       {/* <CreateAccount /> */}
---
+++ front/src/components/CreateNFT/CreateNFT.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./CreateNFT.css";
+
+const CreateNFT = () => {
+    return (
+        <>
+            <div className="create-nft-wrapper">
+
+            </div>
+        </>
+    )
+}
+
+export default CreateNFT
