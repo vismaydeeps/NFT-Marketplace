8256831a93e6a93e3f8d6aec5681e2f3bd73a072
Dabbin-Giraffe <shashank.kathuri@gmail.com>
2024-12-05 03:17:20 +0530

    done with contract

--- back/contracts/deploy.js
+++ back/contracts/deploy.js
@@ -1,27 +1,42 @@
-// scripts/deploy.js
+// // scripts/deploy.js
+// const hre = require("hardhat");
+
+// async function main() {
+//   // Deploy the NFTMarketplace
+//   const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+//   const marketplace = await NFTMarketplace.deploy();
+//   await marketplace.deployed();
+
+//   console.log("NFTMarketplace deployed to:", marketplace.address);
+
+//   // Save the contract addresses for future reference
+//   const addresses = {
+//     NFTMarketplace: marketplace.address,
+//   };
+
+//   // Save contract address to a file
+//   const fs = require("fs");
+//   fs.writeFileSync('deployedAddresses.json', JSON.stringify(addresses, null, 2));
+// }
+
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
+
+
 const hre = require("hardhat");
 
 async function main() {
-  // Deploy the NFTMarketplace
-  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
-  const marketplace = await NFTMarketplace.deploy();
-  await marketplace.deployed();
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy();
 
-  console.log("NFTMarketplace deployed to:", marketplace.address);
+  await lock.deployed();
 
-  // Save the contract addresses for future reference
-  const addresses = {
-    NFTMarketplace: marketplace.address,
-  };
+  console.log(
+    `Something deploy console`
+  )
 
-  // Save contract address to a file
-  const fs = require("fs");
-  fs.writeFileSync('deployedAddresses.json', JSON.stringify(addresses, null, 2));
 }
-
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
